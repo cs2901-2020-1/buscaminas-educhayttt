1ccbaa586c64d024563b3e871bcf2f52143831cb
Nadnus <fernando.delosheros@utec.edu.pe>
2020-06-04 11:43:36 -0500

    clase base

--- src/main/java/cs_utec/cs2901/Celda.java
+++ src/main/java/cs_utec/cs2901/Celda.java
@@ -1,9 +1,26 @@
 package cs_utec.cs2901;
 
 public class Celda {
-    boolean bomba;
-    boolean flagged;
-    public boolean click(){}
-    public boolean flag(){};
+    boolean revelada = false;
+    boolean bomba = false;
+    boolean flagged = false;
+    int adjacent = 0;
+    public void setAdjacent(int new_adj){
+        adjacent = new_adj;
+    };
+    public boolean setBomba(){
+        if (bomba = false){
+          bomba = true;
+          return false;
+        }else {return true;}
+    }
+    public boolean click(){
+        if (bomba){
+            return true;
+        }else {return false;}
+    }
+    public void flag(){
+        flagged = true;
+    };
 
 }
