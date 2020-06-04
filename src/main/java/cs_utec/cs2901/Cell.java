70712074a7ae4b879af783db342759975f0868cc
Miguel Yurivilca <miguel.yurivilca.16@gmail.com>
2020-06-04 12:03:58 -0500

    minor changes

--- src/main/java/cs_utec/cs2901/Cell.java
+++ src/main/java/cs_utec/cs2901/Cell.java
@@ -1,12 +1,32 @@
 package cs_utec.cs2901;
 
 public class Cell {
-        boolean bomba;
-        boolean flagged;
-        public boolean click(){
+    boolean revelada = false;
+    boolean bomba = false;
+    boolean flagged = false;
+    int adjacent = 0;
 
-        }
-        public boolean flag(){
+    public void setAdjacent(int new_adj) {
+        adjacent = new_adj;
+    }
 
+    public boolean setBomba() {
+        if (bomba = false) {
+            bomba = true;
+            return false;
+        } else {
+            return true;
         }
+    }
+
+    public boolean click() {
+        return bomba;
+    }
+
+    public void flag() {
+        flagged = true;
+    }
+
+    Cell() {
+    }
 }
